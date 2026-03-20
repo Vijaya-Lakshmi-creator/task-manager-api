b5bfc8e587aa606e5f6e8cf430968e8d42e09460
vijaya lakshmi <24jr1a05eu@gmail.com>
2026-03-20 22:59:24 +0530

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+import cors from "cors";
 const express = require("express")
 const connectDB = require("./config/db")
 const taskRoutes = require("./routes/taskRoutes")
 require("dotenv").config();
 
 const app = express()
+app.use(cors());
 
 app.use(express.json())
 
